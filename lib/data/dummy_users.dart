6525619416793582207b4ebd3a92f96eec8a751e
Nickael <qaznickael@protonmail.com>
2020-08-20 19:31:59 -0300

    crud test

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@maria.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'maria@maria.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'maria@maria.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/25/21/14/woman-1353211_960_720.png'
+  ),
+};
---
+++ lib/views/user_form.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
+
+class UserForm extends StatelessWidget {
+
+  final _form = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
+
+  void _loadFormData(User user) {
+    _formData['id'] = user.id;
+    _formData['name'] = user.name;
+    _formData['email'] = user.email;
+    _formData['avatarUrl'] = user.avatarUrl;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final User user = ModalRoute.of(context).settings.arguments;
+
+    _loadFormData(user);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Formulário de Usuário'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save), 
+            onPressed: () {
+              _form.currentState.save();
+              Provider.of<Users>(context, listen: false).put(User(
+                id: _formData['id'],
+                name: _formData['name'],
+                email: _formData['email'],
+                avatarUrl: _formData['avatarUrl'],
+              ));
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Nome'),
+                onSaved: (value) => _formData['name'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                onSaved: (value) => _formData['email'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'URL do Avatar'),
+                onSaved: (value) => _formData['avatarUrl'] = value,
+              ),
+            ],
+          )
+        )
+      ),
+    );
+  }
+}
